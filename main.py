57edb1d3151ca869df4b98ecafb5d1ae37a88648
js <jiseong370@gmail.com>
2022-08-05 19:35:49 +0900

    streamend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 topics = [
     {"id":1, "title":"commit", "view":100},
     {"id":2, "title":"push", "view":200},
-    {"id":3, "title":"pull", "view":50}
+    {"id":3, "title":"pull", "view":50},
     {"id":4, "title":"review", "end":0}
 ]
 topics
